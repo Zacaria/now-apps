c5f8f5f0332a81ade2a08ca3e226f526ee42bcfe
Zacaria <z@havesomecode.io>
2018-11-24 14:16:55 +0100

    [UP] Had to export express app to make it work ...

--- node/index.js
+++ node/index.js
@@ -2,4 +2,8 @@ const app = require('express')()
 app.get('*', (req, res) => {
   res.send('Hello from Express.js!');
 })
+
+console.log(process.env.NODE_PORT);
 app.listen();
+
+module.exports = app;
